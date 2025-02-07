53c7b2424cd92a1999c7db7743e11322effd8fac
Weeabamboozled <96398028+Weeabamboozled@users.noreply.github.com>
2025-02-07 15:01:57 -0600

    Add files via upload

---
+++ util.modules/checkAdministrator.psm1
@@ -0,0 +1,9 @@
+﻿# Function to check if the script is running as an Administrator
+function checkAdministrator {
+    $currentUser = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
+    if (-not $currentUser.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+        Write-Error "This script requires administrative privileges. Please run as Administrator."
+        exit 1
+    }
+}
+Export-ModuleMember checkAdministrator
